be7c93000e96f1dbc00c8fb902a0c3e2afdf70a8
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-18 16:06:56 +0200

    Einfügen der ASCII-Art und Wilkommensnachricht

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,7 +1,22 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie Ihren Namen ein:");
+        String name = scanner.nextLine();
+        System.out.println("     ,     ,");
+        System.out.println("    [o]---[o]");
+        System.out.println("       \\_/");
+        System.out.println("     .-'-'-.");
+        System.out.println("    /       \\");
+        System.out.println("   |  |   |  |      " + "Wilkommen bei Robo-Wars " + name + "!");
+        System.out.println("   |  |   |  |");
+        System.out.println("    \\_______/");
+        System.out.println("       | |");
+        System.out.println("       | |");
+        System.out.println("      /   \\");
     }
 }
