a747735762d925551f48fdb3fa32544e1805450c
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-09-26 14:53:25 +0200

    Intro und Roboname funktion hinzugefügt/erweitert.

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        intro();
+
+        System.out.println("Sie haben folgenden Robotor ausgewählt: " + roboName());
+
+
+        int breite = 15;
+        int hoehe = 10;
+        int pos_X = 14;
+        int pos_Y = 9;
+        printBoard(breite, hoehe, pos_X, pos_Y, roboName());
+
+    }
+    public static void intro () {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bitte geben Sie Ihren Namen ein:");
         String name = scanner.nextLine();
@@ -18,25 +31,6 @@ public class Main {
         System.out.println("       | |");
         System.out.println("       | |");
         System.out.println("      /   \\");
-
-        System.out.println("Bitte wählen Sie Ihren Roboter aus:");
-        System.out.println("Geben Sie 1 ein für X");
-        System.out.println("Geben Sie 2 ein für #");
-        System.out.println("Geben Sie 3 ein für $");
-
-        int roboAuswahl = 0;
-        while (roboAuswahl <= 0 || roboAuswahl >= 4) {
-            roboAuswahl = scanner.nextInt();
-        }
-
-        System.out.println("Sie haben folgenden Robotor ausgewählt: " + roboName(roboAuswahl));
-
-        int breite = 15;
-        int hoehe = 10;
-        int pos_X = 14;
-        int pos_Y = 9;
-        printBoard(breite, hoehe, pos_X, pos_Y, roboName(roboAuswahl));
-
     }
     public static void printBoard(int breite, int hoehe, int pos_X, int pos_Y, String roboName) {
 
@@ -51,7 +45,17 @@ public class Main {
             System.out.println();
         }
     }
-    public static String roboName (int roboAuswahl) {
+    public static String roboName () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte wählen Sie Ihren Roboter aus:");
+        System.out.println("Geben Sie 1 ein für X");
+        System.out.println("Geben Sie 2 ein für #");
+        System.out.println("Geben Sie 3 ein für $");
+
+        int roboAuswahl = 0;
+        while (roboAuswahl <= 0 || roboAuswahl >= 4) {
+            roboAuswahl = scanner.nextInt();
+        }
         if (roboAuswahl == 1) {
             return "[X]";
         } else if (roboAuswahl == 2) {
