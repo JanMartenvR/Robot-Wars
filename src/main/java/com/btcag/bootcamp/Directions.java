6105f19ff856e7c36726b29e52bd05acbb0b1f17
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-11-07 15:40:22 +0100

    Enum aktualisiert um Bewegung in alle Richtungen darzustellen.

--- src/main/java/com/btcag/bootcamp/Directions.java
+++ src/main/java/com/btcag/bootcamp/Directions.java
@@ -1,12 +1,21 @@
 package com.btcag.bootcamp;
 
 public enum Directions {
-    NORD,
-    NORDOST,
-    OST,
-    SUEDOST,
-    SUED,
-    SUEDWEST,
-    WEST,
-    NORDWEST;
+    NORD(0,-1),
+    NORDOST(1,-1),
+    OST(1,0),
+    SUEDOST(1,1),
+    SUED(0,1),
+    SUEDWEST(-1,1),
+    WEST(-1,0),
+    NORDWEST(-1,-1);
+
+    private final int x;
+    private final int y;
+
+
+    Directions(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
