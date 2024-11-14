40a942d8a9b2a6e52d4f6072a7ea158a85d267d9
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-11-14 16:15:02 +0100

    Switch statt IF

--- src/main/java/com/btcag/bootcamp/Views/AskSkillPointView.java
+++ src/main/java/com/btcag/bootcamp/Views/AskSkillPointView.java
@@ -9,25 +9,28 @@ public class AskSkillPointView {
         int spend = 0;
         int skillpoints = 10;
         Scanner scanner = new Scanner(System.in);
-        char[] points = new char[10];
         while (spend < skillpoints) {
             System.out.println("Bitte verteilen Sie 10 Skillpoints auf die folgen Attribute Ihres Roboters: \n Bewegungsrate: "+ spieler.getMovementspeed() + "\n Schaden: "+ spieler.getDamage() + "\n Gesundheit: " + spieler.getHp() + "\n Reichweite: " + spieler.getRange());
             System.out.println("Drücken Sie m für Movementspeed, d für Damage, g für Gesundheit und r für Reichweite. Sie können noch "+(skillpoints-spend)+" Punkte verteilen.");
             String input = scanner.next();
-            if (input.equals("m")) {
-                spieler.setMovementspeed(spieler.getMovementspeed()+1);
-                spend += 1;
-            } else if (input.equals("d")) {
-                spieler.setDamage(spieler.getDamage()+1);
-                spend += 1;
-            } else if (input.equals("g")) {
-                spieler.setHp(spieler.getHp()+1);
-                spend += 1;
-            } else if (input.equals("r")) {
-                spieler.setRange(spieler.getRange()+1);
-                spend += 1;
-            } else {
-                System.out.println("Eingabe ungültig.");
+            switch (input) {
+                case "m" -> {
+                    spieler.setMovementspeed(spieler.getMovementspeed() + 1);
+                    spend += 1;
+                }
+                case "d" -> {
+                    spieler.setDamage(spieler.getDamage() + 1);
+                    spend += 1;
+                }
+                case "g" -> {
+                    spieler.setHp(spieler.getHp() + 1);
+                    spend += 1;
+                }
+                case "r" -> {
+                    spieler.setRange(spieler.getRange() + 1);
+                    spend += 1;
+                }
+                default -> System.out.println("Eingabe ungültig.");
             }
 
         }
