399a6f9b34d74edd2cafe4e4957a7280ffdf9223
JAROSSUM <janmarten.vanrossum@btc-ag.com>
2024-12-05 13:37:44 +0100

    Datenbankstruktur über hibernate von Freddy

--- src/main/java/com/btcag/bootcamp/Service/GameService.java
+++ src/main/java/com/btcag/bootcamp/Service/GameService.java
@@ -3,11 +3,25 @@ package com.btcag.bootcamp.Service;
 import com.btcag.bootcamp.Entities.Game;
 import com.btcag.bootcamp.Interfaces.IGameService;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static com.btcag.bootcamp.Service.ConnectorDB.dbConnector.connect;
+
 public class GameService implements IGameService {
 
     @Override
     public Game getGame(int gameId) {
         //get the game objekt from database with the ID gameId
+        String sql = "CREATE TABLE IF NOT EXISTS User (" + "id INTEGER PRIMARY KEY AUTOINCREMENT, " + "username TEXT NOT NULL, " + "password TEXT NOT NULL);";
+        try (Connection conn = connect(); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabelle 'User' wurde erfolgreich erstellt.");
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Erstellen der Tabelle 'User'.");
+            e.printStackTrace();
+        }
         Game game = new Game();
         return game;
     }
--- src/main/java/com/btcag/bootcamp/Service/MapService.java
+++ src/main/java/com/btcag/bootcamp/Service/MapService.java
@@ -3,11 +3,35 @@ package com.btcag.bootcamp.Service;
 import com.btcag.bootcamp.Entities.Map;
 import com.btcag.bootcamp.Interfaces.IMapService;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static com.btcag.bootcamp.Service.ConnectorDB.dbConnector.connect;
+import static javax.swing.UIManager.getInt;
+
 
 public class MapService implements IMapService {
 
     @Override
     public Map gameMap(int gameId) {
-        return null;
+        //get map des Spiels mit der GameId
+        //SELECT mapID FROM game WHERE GameId == gameId
+        String sql = "SELECT mapID FROM game WHERE GameId == "+ gameId +";";
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Map map = new Map(int rs.getInt("gameId",));
+                System.out.println("ID: " + rs.getInt("id") + "\t" +
+                        "Username: " + rs.getString("username") + "\t" +
+                        "Password: " + rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Fehler beim Abrufen des Games.");
+            e.printStackTrace();
+        }
+        return map;
     }
 }
